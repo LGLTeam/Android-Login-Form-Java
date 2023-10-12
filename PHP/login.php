85fdd988237d3b2108c7e5f3362ece33d67d5ae9
SharHtetAung <126490421+SharHtetAung@users.noreply.github.com>
2023-10-12 23:23:23 +0630

    Update login.php
    New login panel style.😍

--- PHP/login.php
+++ PHP/login.php
@@ -1,18 +1,17 @@
 <?PHP
 $data = '';
 
-$user = 'user';
-$pass = 'pass';
+$user = 'key';
 
 if (!empty($_REQUEST) ) {
-    if (!empty($_REQUEST[$user]) && !empty($_REQUEST[$pass])) {
-        if ($_REQUEST[$user] == 'testuser' && $_REQUEST[$pass] == 'testpass') {
-            $data = '1|'.md5($_REQUEST[$user] . $_REQUEST[$pass]).'|User: ' . $_REQUEST[$user] . ' successfully logged in!';
+    if (!empty($_REQUEST[$user])) {
+        if ($_REQUEST[$user] == 'testkey') {
+            $data = '1|'.md5($_REQUEST[$user]).'|Successfully logged in!';
         }else{
-            $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Incorrect username or password!';
+            $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Incorrect key // Get new key!';
         }
     }else{
-        $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Fill in username and password!';
+        $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Input your key first!';
     }
 }
 
